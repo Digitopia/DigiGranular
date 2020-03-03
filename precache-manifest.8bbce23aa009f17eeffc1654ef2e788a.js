406687b60ed7e8ba1752b65dc3ec76537b6afb77
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-03 10:35:03 +0000

    Deploy Digitopia/DigiGranular to github.com/Digitopia/DigiGranular.git:gh-pages

---
+++ precache-manifest.8bbce23aa009f17eeffc1654ef2e788a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f426c0030a58f4237743",
+    "url": "/DigiGranular/css/app.da4f502d.css"
+  },
+  {
+    "revision": "939e5f2895b7ff6bd771b914056d4412",
+    "url": "/DigiGranular/index.html"
+  },
+  {
+    "revision": "f426c0030a58f4237743",
+    "url": "/DigiGranular/js/app.0a1fa6c7.js"
+  },
+  {
+    "revision": "770a3129a18fbbb03a17",
+    "url": "/DigiGranular/js/chunk-vendors.4b6ea061.js"
+  },
+  {
+    "revision": "12725d91e7fec281451af61c556cc462",
+    "url": "/DigiGranular/manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/DigiGranular/robots.txt"
+  },
+  {
+    "revision": "7bf82e915c2736de23f47d4d015f09db",
+    "url": "/DigiGranular/sounds/bird.wav"
+  },
+  {
+    "revision": "5c432e2eb6f844483c57ee4a9a59da88",
+    "url": "/DigiGranular/sounds/bonang.wav"
+  },
+  {
+    "revision": "2285ef316dfe03bf083f31b86de3d43c",
+    "url": "/DigiGranular/sounds/guitar.wav"
+  },
+  {
+    "revision": "91405f5111c2403c5ac6f573946857b8",
+    "url": "/DigiGranular/sounds/speech.wav"
+  }
+]);
